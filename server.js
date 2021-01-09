fed14fbf92703c2fb01c653647997770d87f986e
aniketlodh <aniketlodh6@gmail.com>
2021-01-09 21:43:30 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+require("dotenv").config()
+var express=require("express");
+var app=express();
+var bodyParser=require("body-parser");
+var PORT=3000 || process.env.PORT;
+var Insta = require('instamojo-nodejs');
+Insta.setKeys(process.env.API_KEY,process.env.AUTH_KEY );
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+Insta.isSandboxMode(true);
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+app.get("/donate",(req,res)=>{
+    res.render("donate")
+})
+
+app.post("/pay",(req,res)=>{
+    
+    var name=req.body.name;
+    var email=req.body.email;
+    var amount=req.body.amount;
+    
+    var data = new Insta.PaymentData();
+    
+    data.purpose = "Child Welfare";
+    data.amount = amount;                 
+    data.name=name;
+    data.email=email;
+    data.setRedirectUrl("http://nodepayment.herokuapp/success");
+    data.send_email ="True"   
+    Insta.createPayment(data, function(error, response) {
+        if (error) {
+            res.send("Error occured");
+        } else {
+            console.log(response);
+            res.send("Please check your email to confirm payment");
+        }
+    });
+})
+
+app.get("/success",(req,res)=>{
+    res.send("Payment was successful. Please check your email for invoice");
+})
+app.listen(PORT,()=>{
+    console.log(`Server started at ${PORT}`);
+})
